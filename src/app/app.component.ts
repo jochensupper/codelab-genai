3a37733bcaf090a9a3aa9d7ba9863de634958d4c
jochen Supper <jochensupper@gmail.com>
2024-09-24 21:02:43 +0000

    add latest changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,43 @@
-import { Component } from '@angular/core';
+// app.component.ts
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, FormsModule],
+    template: `
+        <section>
+            <input
+                type="text"
+                placeholder="dog"
+                [(ngModel)]="animal"
+                class="text-black border-2 p-2 m-2 rounded"
+            />
+            <button
+                (click)="getNewFunFacts()"
+            >
+                Get New Fun Facts
+            </button>
+            <ol>
+                @for(fact of facts(); track fact) {
+                    <li>{{fact}}</li>  
+                } @empty {
+                    <li>No facts are available</li>
+                }
+            </ol>
+        </section>
+    `,
+    styles: '',
 })
 export class AppComponent {
-  title = 'codelab-genai';
+    animal = '';
+    facts = signal<string[]>([]);
+
+    getNewFunFacts() {
+        fetch(`/api/facts?animal=${this.animal}`).then(response => response.json()).then(facts => {
+            this.facts.set(facts);
+        });
+    }
 }
